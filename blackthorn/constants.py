231e692a62e095788a13168939d766b4e6181a3d
Logan A. Morrison <loganmorrison99@gmail.com>
2022-11-09 18:29:32 -0800

    pylinting `constants.py`

--- blackthorn/constants.py
+++ blackthorn/constants.py
@@ -1,20 +1,24 @@
+"""Useful constants for working with RHN models."""
+
+# pylint: disable=invalid-name
+
 import enum
 
 import numpy as np
 
 
 class Gen(enum.IntEnum):
+    """Enumeration for fermion generations."""
+
     Fst = 0
     Snd = 1
     Trd = 2
 
     def __repr__(self):
-        if self == Gen.Fst:
-            return "1"
-        elif self == Gen.Snd:
-            return "2"
-        else:
-            return "3"
+        return self.name
+
+    def __str__(self):
+        return self.name
 
 
 # Fermi constant in GeV^-2
@@ -28,37 +32,37 @@ SW = 0.480853
 # Cosine of the weak mixing angle
 CW = 0.876801
 
-_ckm_lam = 0.22650
-_ckm_a = 0.790
-_ckm_rho = 0.141
-_ckm_eta = 0.357
+_CKM_LAM = 0.22650
+_CKM_A = 0.790
+_CKM_RHO = 0.141
+_CKM_ETA = 0.357
 
-_s12 = _ckm_lam
-_s23 = _ckm_a * _ckm_lam**2
-_s13 = _ckm_a * _ckm_lam**3 * np.sqrt(_ckm_rho**2 + _ckm_eta**2)
-_delta = np.arctan2(_ckm_eta, _ckm_rho)
-_c12 = np.sqrt(1 - _s12**2)
-_c13 = np.sqrt(1 - _s13**2)
-_c23 = np.sqrt(1 - _s23**2)
+_SIN12 = _CKM_LAM
+_SIN23 = _CKM_A * _CKM_LAM**2
+_SIN13 = _CKM_A * _CKM_LAM**3 * np.sqrt(_CKM_RHO**2 + _CKM_ETA**2)
+_DELTA = np.arctan2(_CKM_ETA, _CKM_RHO)
+_COS12 = np.sqrt(1 - _SIN12**2)
+_COS13 = np.sqrt(1 - _SIN13**2)
+_COS23 = np.sqrt(1 - _SIN23**2)
 
 # CKM matrix element of u-d
-CKM_UD = _c12 * _c13
+CKM_UD = _COS12 * _COS13
 # CKM matrix element of u-s
-CKM_US = _c13 * _s12
+CKM_US = _COS13 * _SIN12
 # CKM matrix element of u-b
-CKM_UB = _s13 * np.exp(1j * _delta)
+CKM_UB = _SIN13 * np.exp(1j * _DELTA)
 # CKM matrix element of c-d
-CKM_CD = -(_c23 * _s12) - _c12 * np.exp(1j * _delta) * _s13 * _s23
+CKM_CD = -(_COS23 * _SIN12) - _COS12 * np.exp(1j * _DELTA) * _SIN13 * _SIN23
 # CKM matrix element of c-s
-CKM_CS = _c12 * _c23 - np.exp(1j * _delta) * _s12 * _s13 * _s23
+CKM_CS = _COS12 * _COS23 - np.exp(1j * _DELTA) * _SIN12 * _SIN13 * _SIN23
 # CKM matrix element of c-b
-CKM_CB = _c13 * _s23
+CKM_CB = _COS13 * _SIN23
 # CKM matrix element of t-d
-CKM_TD = -(_c12 * _c23 * np.exp(1j * _delta) * _s13) + _s12 * _s23
+CKM_TD = -(_COS12 * _COS23 * np.exp(1j * _DELTA) * _SIN13) + _SIN12 * _SIN23
 # CKM matrix element of t-s
-CKM_TS = -(_c23 * np.exp(1j * _delta) * _s12 * _s13) - _c12 * _s23
+CKM_TS = -(_COS23 * np.exp(1j * _DELTA) * _SIN12 * _SIN13) - _COS12 * _SIN23
 # CKM matrix element of t-b
-CKM_TB = _c13 * _c23
+CKM_TB = _COS13 * _COS23
 
 CKM = np.array(
     [
